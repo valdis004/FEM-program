00621f47cc6b3e4279bffd1deed1fdf8fe6a7488
valdis004 <vladan004@gmail.com>
2026-01-08 18:16:01 +0300

    create files

---
+++ out/build/Clang 18.1.3 x86_64-pc-linux-gnu/CMakeFiles/FEM-program.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FEM-program.dir/main.cpp.o"
+  "CMakeFiles/FEM-program.dir/main.cpp.o.d"
+  "FEM-program"
+  "FEM-program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FEM-program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
